c5a42d4d7f75e26561fd89fd105c02df8fc9afb3
mudassir <mudassir95546589@gmail.com>
2024-06-19 12:15:20 +0530

    first commit

---
+++ controllers/getCategoryWiseProduct.js
@@ -0,0 +1,28 @@
+const productModel = require("../models/productModel")
+
+const getCategoryWiseProduct=async(req,res)=>{
+    try {
+        const {category}=req?.body
+
+        const product = await productModel.find(category)
+        console.log(product)
+
+        res.json({
+            data:product,
+            message:'Product',
+            success:true,
+            error:false
+        })
+
+
+
+    } catch (error) {
+          res.json({
+            message:error.message||error,
+            error:true,
+            success:false
+        })
+    }
+}
+
+module.exports=getCategoryWiseProduct
